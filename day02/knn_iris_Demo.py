66c436d7741dcaa398d2e5f68960d8421aa40e1e
youtatu <youtatu@163.com>
2024-04-20 11:46:36 +0800

    day02代码 朴素贝叶斯算法+概率为0时利用拉普拉斯平滑系数处理

--- day02/knn_iris_Demo.py
+++ day02/knn_iris_Demo.py
@@ -1,7 +1,9 @@
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris, fetch_20newsgroups
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 
 def knn_iris_demo():
@@ -49,7 +51,8 @@ def knn_iris_gscv():
     estimator = KNeighborsClassifier()
     # 加入网格收缩与交叉验证模型优化
     param_grid = {'n_neighbors': [1, 3, 5, 7, 9, 11, 13, 17, 19, 21]}
-    estimator = GridSearchCV(estimator=estimator, param_grid=param_grid, n_jobs=-1, cv=10)
+    estimator = GridSearchCV(estimator, param_grid, n_jobs=-1, cv=10)
+    # 对训练集进行计算,找到最佳组合
     estimator.fit(x_train, y_train)
 
     # 5.模型评估
@@ -72,9 +75,44 @@ def knn_iris_gscv():
     return None
 
 
+def nb_new():
+    """
+    用朴素贝叶斯算法对新闻进行分类
+    :return:
+    """
+    # 1.获取数据
+    news = fetch_20newsgroups(subset='train')
+
+    # 2. 划分数据集
+    x_train, x_test, y_train, y_test = train_test_split(news.data, news.target, random_state=6)
+
+    # 3. 特征工程 文本特征抽取-tfidf
+    transfer = TfidfVectorizer()
+    x_train = transfer.fit_transform(x_train)
+    x_test = transfer.transform(x_test)
+
+    # 4. 朴素贝叶斯预估器流程
+    # alpha 默认为1.0 - 0.84 自己调这个参数 0.01-0.90,优化准确率
+    estimator = MultinomialNB(alpha=0.01)
+    estimator.fit(x_train, y_train)
+
+    # 5. 模型评估
+    y_predict = estimator.predict(x_test)
+    print("y_predict\n", y_predict)
+    print("直接评估\n", y_predict == y_test)
+
+    accuracy = estimator.score(x_test, y_test)
+    print("准确率:\n", accuracy)
+
+    return None
+
+
 if __name__ == '__main__':
     # 代码1: 使用KNN对鸢尾花分类
     # knn_iris_demo()
 
     # 代码2: 使用KNN对鸢尾花分类,并进行调优
-    knn_iris_gscv()
+    # knn_iris_gscv()
+
+    # 代码3: 用朴素贝叶斯算法对数据分类
+    nb_new()
